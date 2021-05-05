0c74ef4b38dadd5f533f320c2c0d2aeb91bfac4f
TheGoatGod <56883303+TheGoatGod@users.noreply.github.com>
2021-05-05 16:53:18 +0100

    Update style-json.ps1

--- style-json.ps1
+++ style-json.ps1
@@ -2,6 +2,6 @@ Write-Output "JSON formatting script begins."
 $scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 Set-Location -Path (Join-Path -Path $scriptDir -ChildPath "..")
 $blacklist = Get-Content "early-survival-mod\json_blacklist" | Resolve-Path -Relative
-$files = Get-ChildItem -Recurse -Include *.json "early-survival-mod\" | Resolve-Path -Relative | ?{$blacklist -notcontains $_}
+$files = Get-ChildItem -Recurse -Include *.json "early-survival-mod\Goats_Early_Survival" | Resolve-Path -Relative | ?{$blacklist -notcontains $_}
 $files | ForEach-Object { Invoke-Expression "early-survival-mod\tools\format\json_formatter.exe $_" }
 Write-Output "JSON formatting script ends."
